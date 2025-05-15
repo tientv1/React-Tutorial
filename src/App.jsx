56af127b15a3357ffb6a7b3b22517d7d3190f42e
Tien Tran Van <tientranvan19052004@gmail.com>
2025-05-15 23:58:48 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
-function App() {}
+import Header from "./Header";
+import Footer from "./Footer";
+import Food from "./Food";
+
+function App() {
+    return (
+        <>
+            <Header />
+            <Footer />
+            <Food />
+        </>
+    );
+}
 
 export default App;
